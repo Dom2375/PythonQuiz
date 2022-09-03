d431f79b9f3ea97fc7158f6bd10cda86a65c177b
Dom2375 <dominic.23773@hotmail.com>
2022-09-03 16:59:05 +0100

    format

--- pythonquiz.py
+++ pythonquiz.py
@@ -1,9 +1,7 @@
 #pythonquiz.py
+from string import ascii_lowercase
 
 QUESTIONS = {
-    "Which keyword do you use to loop over a given list of elements": [
-        "for", "while", "each", "loop"
-    ],
     "What's the purpose of the built-in zip() function": [
         "To iterate over two or more sequences at the same time",
         "To combine several strings into one",
@@ -13,22 +11,24 @@ QUESTIONS = {
     "What's the name of Python's sorting algorithm": [
         "Timsort", "Quicksort", "Merge sort", "Bubble sort"
     ],
+    "What does dict.get(key) return if key isn't found in dict": [
+        "None", "key", "True", "False",
+    ]
 }
 
-for question, alternatives in QUESTIONS.items():
+for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
     correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
-    for label, alternative in enumerate(sorted_alternatives):
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
 
-    answer_label = int(input(f"{question}? "))
-    answer = sorted_alternatives[answer_label]
-
-
+    answer_label = input("\nChoice? ")
+    answer = labeled_alternatives.get(answer_label)
     if answer == correct_answer:
-        print("Correct!")
+        print("⭐ Correct! ⭐")
     else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}")        
-
+        print(f"The answer is {correct_answer!r}, not {answer!r}")     
 
 input("Press Enter to exit...")
